f9e5ca89d9fb7246cc43af2d3699c2181c8e4725
nathan-guimaraes <nathan.guimaraes@hotmail.com.br>
2025-05-20 18:47:12 -0300

    Update services and database configuration
    - Changed ports in `docker-compose.yml` for better management.
    - Updated migration assembly reference in `DefaultContext.cs`.
    - Added `CreatedAt` and `UpdatedAt` fields to `User` entity.
    - Modified connection string in `appsettings.json` for PostgreSQL.

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20250520182744_UpdateUserModel.Designer.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20250520182744_UpdateUserModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Ambev.DeveloperEvaluation.ORM;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Ambev.DeveloperEvaluation.ORM.Migrations
 {
     [DbContext(typeof(DefaultContext))]
-    partial class DefaultContextModelSnapshot : ModelSnapshot
+    [Migration("20250520182744_UpdateUserModel")]
+    partial class UpdateUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -36,14 +39,6 @@ namespace Ambev.DeveloperEvaluation.ORM.Migrations
                         .IsRequired()
                         .HasMaxLength(100)
                         .HasColumnType("character varying(100)");
-
-                    b.Property<string>("FirstName")
-                        .IsRequired()
-                        .HasColumnType("text");
-
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasColumnType("text");
 
                     b.Property<string>("Password")
                         .IsRequired()
