6a88796d939655f26fb7f91da3fdefdeef80897d
nathan-guimaraes <nathan.guimaraes@hotmail.com.br>
2025-05-24 00:15:40 -0300

    Make properties in CreateSaleResult and CreateSaleResponse writable

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
@@ -5,12 +5,12 @@ namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
 public class CreateSaleResult
 {
     public string Id { get; set; }
-    public long Number { get; }
-    public Guid UserId { get; }
-    public DateTime SaleDate { get; }
-    public List<SaleItem> Items { get; } = [];
-    public decimal TotalSaleAmount { get; }
-    public bool IsCanceled { get; }
-    public decimal TotalSaleDiscount { get; }
-    public string Branch { get; }
+    public long Number { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime SaleDate { get; set; }
+    public List<SaleItem> Items { get; set; } = [];
+    public decimal TotalSaleAmount { get; set; }
+    public bool IsCanceled { get; set; }
+    public decimal TotalSaleDiscount { get; set; }
+    public string Branch { get; set; }
 }
